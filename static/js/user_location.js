9d6304ad7802cba45a078b80753a3b4e6c165107
Software-Evoluzn <165126447+Software-Evoluzn@users.noreply.github.com>
2025-03-20 11:24:11 +0530

    Shilpa New Changes

--- static/js/user_location.js
+++ static/js/user_location.js
@@ -1,35 +1,50 @@
 let map, userMarker;
 
 function initMap() {
-    // Create the map but don't set the center yet
     map = new google.maps.Map(document.getElementById("map"), {
         zoom: 12
     });
 
-    // Try to get the current location
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            const userLocation = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                const userLocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
 
-            // Set the map's center to the user's location
-            map.setCenter(userLocation);
+                map.setCenter(userLocation);
 
-            // Create a marker for the user's current location
-            if (userMarker) {
-                userMarker.setPosition(userLocation);
-            } else {
-                userMarker = new google.maps.Marker({
-                    position: userLocation,
-                    map: map,
-                    title: "You are here!"
-                });
+                if (userMarker) {
+                    userMarker.setPosition(userLocation);
+                } else {
+                    userMarker = new google.maps.Marker({
+                        position: userLocation,
+                        map: map,
+                        title: "You are here!"
+                    });
+                }
+            },
+            function (error) {
+                console.error("Geolocation error:", error);
+
+                switch (error.code) {
+                    case error.PERMISSION_DENIED:
+                        alert("User denied the request for Geolocation.");
+                        break;
+                    case error.POSITION_UNAVAILABLE:
+                        alert("Location information is unavailable.");
+                        break;
+                    case error.TIMEOUT:
+                        alert("The request to get user location timed out.");
+                        break;
+                    case error.UNKNOWN_ERROR:
+                    default:
+                        alert("An unknown error occurred.");
+                        break;
+                }
             }
-        }, function () {
-            alert("Error: The Geolocation service failed.");
-        });
+        );
     } else {
         alert("Error: Your browser doesn't support geolocation.");
     }
